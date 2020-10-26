91791644f15c8bafa47691bb47c335f639485953
AndreiSamorokov <andreiv0202@gmail.com>
2020-10-27 05:40:13 +0900

    make rute

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+
+app.get('/csvread', (req, res) => {
+  res.send('Hello World--csv read!')
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
